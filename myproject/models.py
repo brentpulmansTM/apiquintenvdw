3d32f60a86a9293fb9f65958af698125b7fae8b4
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2023-11-09 22:35:46 +0100

    default folder download

--- myproject/models.py
+++ myproject/models.py
@@ -8,14 +8,14 @@ class Star_wars(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
-    faction_id = Column(Integer)
+    faction_id = Column(Integer, ForeignKey("faction.id"))
     lightsaber_Wielder = Column(Boolean, default=True)
     age = Column(Integer)
     species = Column(String)
     addition = Column(String)
 
 
-class Star_wars(Base):
+class Faction(Base):
     __tablename__ = "faction"
 
     id = Column(Integer, primary_key=True, index=True)
